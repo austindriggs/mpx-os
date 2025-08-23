a359b43292fb2bbca57d0d22f5306a66ef0b8a43
austindriggs <austind26@icloud.com>
2025-08-23 14:12:28 -0400

    noted the things to install

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# prep
+sudo apt update
+sudo apt install -y clang make nasm git binutils-i686-linux-gnu qemu-system-x86 gdb
+
+# next steps
+echo
+echo "Run 'make' and then run './mpx.sh'."
+
