e30acdb2ffc01e79f5c73ed8c3e58ef63400b755
austindriggs <austind26@icloud.com>
2025-08-27 14:00:58 -0400

    austins changes for caleb

--- kernel/comhand.c
+++ kernel/comhand.c
@@ -15,8 +15,8 @@ void trim_Input(char *str){
 void comhand(void)
 {
     // Startup Message
-    const char *welcomeMsg = "\r\nWelcome to MacaroniPenguins OS";
-    const char *introMsg = "\r\nType help to see a list of commands, Type exit to shutdown!\r\n";
+    const char *welcomeMsg = "\r\nWelcome to MacaroniOS";
+    const char *introMsg = "\r\nType help to see a list of commands, Type exit to shutdown!\r\n\r\n";
     sys_req(WRITE, COM1, welcomeMsg, strlen(welcomeMsg));
     sys_req(WRITE, COM1, introMsg, strlen(introMsg));
 
@@ -36,7 +36,7 @@ void comhand(void)
        
        
         /* the following simply echoes the input */
-        sys_req(WRITE, COM1, buf, nread);
+        //sys_req(WRITE, COM1, buf, nread);
         sys_req(WRITE, COM1, "\r\n", 2); // Start new line
 
         // Exit Command
@@ -45,7 +45,7 @@ void comhand(void)
             sys_req(WRITE, COM1, confirmationMsg, strlen(confirmationMsg));
 
         while(1){
-                char confirmation[3] = {0}; // size 1 doesn't work for enter and null terminator
+                char confirmation[2] = {0}; // size 1 doesn't work for enter and null terminator
             
                 // Read y or n
                 int nreadExit = sys_req(READ, COM1, confirmation, sizeof(confirmation));
@@ -54,15 +54,15 @@ void comhand(void)
 
                 // if y or n, then exit or return
                 if (confirmation[0] == 'y' || confirmation[0] == 'Y'){
-                    const char *exitMsg = "Exiting...\r\n";
+                    const char *exitMsg = "\r\nExiting...\r\n";
                     sys_req(WRITE, COM1, exitMsg, strlen(exitMsg));
                     return;
                 } else if (confirmation[0] == 'n' || confirmation[0] == 'N'){
-                    const char *returnMsg = "Returning...\r\n";
+                    const char *returnMsg = "\r\nReturning...\r\n";
                     sys_req(WRITE, COM1, returnMsg, strlen(returnMsg));
                     break;
                 } else {
-                    const char *invalidMsg = "Invalid input. Please type 'Y' or 'n'.\r\n";
+                    const char *invalidMsg = "\r\nInvalid input. Please type 'Y' or 'n'.\r\n";
                     sys_req(WRITE, COM1, invalidMsg, strlen(invalidMsg));
 
                 }
@@ -72,7 +72,7 @@ void comhand(void)
         // Help Command
         else if (strcmp(buf, "help") == 0) {
             const char *helpMsg = 
-            "\r\nList Of Commands:\r\n"
+            "List Of Commands:\r\n"
             "help - list commands\r\n"
             "exit - exit the program\r\n"
             "version - Show OS version\r\n"
@@ -88,7 +88,7 @@ void comhand(void)
 
         // Unknown Input
         else {
-            const char *invalidMsg = "Invalid command. Try again, type 'help' for all commands.\r\n";
+            const char *invalidMsg = "Invalid command. Try again, type 'help' for all commands.\r\n\r\n";
             sys_req(WRITE, COM1, invalidMsg, strlen(invalidMsg));
         }
 
